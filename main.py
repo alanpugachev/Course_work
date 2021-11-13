d206ff5c676cb57733c5a5d9390cc40764249d1c
alanpugachev <aszneesizzle@gmail.com>
2021-11-13 17:45:48 +0300

    Init

---
+++ main.py
@@ -0,0 +1,28 @@
+# imports
+import numpy as np
+from pandas import read_csv
+from numpy.lib.npyio import genfromtxt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import utils
+
+def normalization(data):
+    print(data.dtype)
+
+# main
+training_data = read_csv('training_data.csv')
+training_data = training_data.values
+
+#—prepare data
+answers = training_data[:,0]
+answers = utils.to_categorical(answers)
+data = training_data[:,1:]
+normalization(data)
+
+#——creating AI-model
+# model = Sequential()
+# model.add(Dense(128, activation='sigmoid', input_dim=7))
+# model.add(Dense(64, activation='relu'))
+# model.add(Dense(10, activation='softmax'))
+# model.compile(loss='categorical_crossentropy', optimizer='SGD', metrics=['accuracy'])
+# model.fit(data, answers, batch_size=1, epochs=100, verbose=1)
