873e67b13fd387ea392a140b30c4a6a5fa204665
alanpugachev <aszneesizzle@gmail.com>
2021-11-15 10:11:29 +0300

    Uodate some activations and optimizers

--- main.py
+++ main.py
@@ -33,8 +33,8 @@ print(data)
 
 #——creating AI-model
 model = Sequential()
-model.add(Dense(128, activation='sigmoid', input_dim=7))
+model.add(Dense(128, activation='relu', input_dim=7))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(10, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='SGD', metrics=['accuracy'])
+model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 model.fit(data, answers, batch_size=1, epochs=100, verbose=1)
